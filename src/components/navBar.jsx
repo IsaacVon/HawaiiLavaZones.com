1bb625c28e90a65ecfc1807217a74ffc16ef1be0
Isaac Householder <isaachouseholder@gmail.com>
2020-07-14 11:41:16 -0700

    imported M from materialize

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
+import M from "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
 
 class Nav extends Component {
   state = {};
 
   componentDidMount() {
-    const M = window.M;
+    console.log("nav mounted");
     document.addEventListener('DOMContentLoaded', function() {
       var elems = document.querySelectorAll('.dropdown-trigger');
       var instances = M.Dropdown.init(elems,);
@@ -16,31 +18,31 @@ class Nav extends Component {
   render() {
     return (
       <>
-        <ul id="dropdown1" class="dropdown-content">
+        <ul id="dropdown1" className="dropdown-content">
           <li>
             <a href="/LavaZoneInformation">Lava Zone Information</a>
           </li>
           <Link to="/LavaZoneInformation">
           <li>
-            <a>link</a>
+            React Router
           </li>
           </Link>
-          <li class="divider"></li>
+          <li className="divider"></li>
           <li>
             <a href="#!">three</a>
           </li>
         </ul>
         <nav>
-          <div class="nav-wrapper #b2ebf2 cyan lighten-4">    
+          <div className="nav-wrapper #b2ebf2 cyan lighten-4">    
      
-            <a href="/" class="brand-logo">
+            <a href="/" className="brand-logo">
               Logo
             </a>
   
-            <ul class="right hide-on-med-and-down">
+            <ul className="right hide-on-med-and-down">
               <li>
-                <a class="dropdown-trigger" href="#!" data-target="dropdown1">
-                  More<i class="material-icons right">arrow_drop_down</i>
+                <a className="dropdown-trigger" href="#!" data-target="dropdown1">
+                  More<i className="material-icons right">arrow_drop_down</i>
                 </a>
               </li>
             </ul>
--- src/pages/lavaZoneInformation.jsx
+++ src/pages/lavaZoneInformation.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import M from "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
 
 class LavaZoneInformation extends Component {
   componentDidMount() {
-    const M = window.M;
+    console.log("lava zone mounted");
     document.addEventListener("DOMContentLoaded", function () {
       var elems = document.querySelectorAll(".collapsible");
       var instances = M.Collapsible.init(elems, {});
@@ -12,13 +14,13 @@ class LavaZoneInformation extends Component {
   render() {
     return (
       
-      <div class="container">    
-      <ul class="collapsible">
+      <div className="container">    
+      <ul className="collapsible">
         <li>
-          <div class="collapsible-header #ff7043 deep-orange lighten-1">
-            <i class="material-icons">local_fire_department</i>Zone 1
+          <div className="collapsible-header #ff7043 deep-orange lighten-1">
+            <i className="material-icons">local_fire_department</i>Zone 1
           </div>
-          <div class="collapsible-body #ffccbc deep-orange lighten-5">
+          <div className="collapsible-body #ffccbc deep-orange lighten-5">
             <span>
               Summits and rift zones of Kilauea and Mauna Loa, where vents have
               been repeatedly active in historic time. Boundaries are defined by
@@ -28,10 +30,10 @@ class LavaZoneInformation extends Component {
           </div>
         </li>
         <li>
-          <div class="collapsible-header #ffca28 amber lighten-">
-            <i class="material-icons">local_fire_department</i>Zone 2
+          <div className="collapsible-header #ffca28 amber lighten-">
+            <i className="material-icons">local_fire_department</i>Zone 2
           </div>
-          <div class="collapsible-body #ffecb3 amber lighten-5">
+          <div className="collapsible-body #ffecb3 amber lighten-5">
             <span>
               Areas adjacent to and downslope from Zone 1. Fifteen to twentyfive
               percent of Zone 2 has been covered by lava since 1800, and 25-75
@@ -41,10 +43,10 @@ class LavaZoneInformation extends Component {
           </div>
         </li>
         <li>
-          <div class="collapsible-header #ffecb3 amber lighten-5">
-            <i class="material-icons">filter_drama</i>Zone 3
+          <div className="collapsible-header #ffecb3 amber lighten-5">
+            <i className="material-icons">filter_drama</i>Zone 3
           </div>
-          <div class="collapsible-body">
+          <div className="collapsible-body">
             <span>
               One to five percent of Zone 3 has been covered since 1800, and
               15-75 oercent has been covered within the last 750 years. Areas
@@ -55,10 +57,10 @@ class LavaZoneInformation extends Component {
           </div>
         </li>
         <li>
-          <div class="collapsible-header #fff8e1 #f1f8e9 light-green lighten-5">
-            <i class="material-icons">filter_drama</i>Zone 4
+          <div className="collapsible-header #fff8e1 #f1f8e9 light-green lighten-5">
+            <i className="material-icons">filter_drama</i>Zone 4
           </div>
-          <div class="collapsible-body">
+          <div className="collapsible-body">
             <span>
               Includes all of Hualalai. Moore and others eestimate that large
               eruptions reach the ocean about once every 300 years, a recurrence
@@ -69,10 +71,10 @@ class LavaZoneInformation extends Component {
           </div>
         </li>
         <li>
-          <div class="collapsible-header #f1f8e9 light-green lighten-4">
-            <i class="material-icons">wb_sunny</i>Zone 5
+          <div className="collapsible-header #f1f8e9 light-green lighten-4">
+            <i className="material-icons">wb_sunny</i>Zone 5
           </div>
-          <div class="collapsible-body #f1f8e9 light-green lighten-5">
+          <div className="collapsible-body #f1f8e9 light-green lighten-5">
             <span>
               An area on Kilauea currently protected from rift- or summitderived
               lava flows by north-facing fault scarps, south of Kilauea summit.
@@ -80,10 +82,10 @@ class LavaZoneInformation extends Component {
           </div>
         </li>
         <li>
-          <div class="collapsible-header #f1f8e9 light-green lighten-3">
-            <i class="material-icons">wb_sunny</i>Zone 6
+          <div className="collapsible-header #f1f8e9 light-green lighten-3">
+            <i className="material-icons">wb_sunny</i>Zone 6
           </div>
-          <div class="collapsible-body #f1f8e9 light-green lighten-5">
+          <div className="collapsible-body #f1f8e9 light-green lighten-5">
             <span>
               Two areas on Mauna Loa, both protected from rift- or summitderived
               lava flows. The area south of Mauna Loa summit is analogous to
@@ -98,10 +100,10 @@ class LavaZoneInformation extends Component {
           </div>
         </li>
         <li>
-          <div class="collapsible-header #f1f8e9 light-green lighten-2">
-            <i class="material-icons">beach_access</i>Zone 7
+          <div className="collapsible-header #f1f8e9 light-green lighten-2">
+            <i className="material-icons">beach_access</i>Zone 7
           </div>
-          <div class="collapsible-body #f1f8e9 light-green lighten-5">
+          <div className="collapsible-body #f1f8e9 light-green lighten-5">
             <span>
               The younger part of the dormant volcano Mauna Kea. Twenty percent
               of this area was covered by lave 3,500-5,000 years ago.
@@ -109,10 +111,10 @@ class LavaZoneInformation extends Component {
           </div>
         </li>
         <li>
-          <div class="collapsible-header #f1f8e9 light-green lighten-2">
-            <i class="material-icons">beach_access</i>Zone 8
+          <div className="collapsible-header #f1f8e9 light-green lighten-2">
+            <i className="material-icons">beach_access</i>Zone 8
           </div>
-          <div class="collapsible-body #f1f8e9 light-green lighten-5">
+          <div className="collapsible-body #f1f8e9 light-green lighten-5">
             <span>
               The remaining part of Mauna Kea. Only a few percent of this area
               has been covered by lava in the last 10,000 years.
@@ -120,10 +122,10 @@ class LavaZoneInformation extends Component {
           </div>
         </li>
         <li>
-          <div class="collapsible-header #f1f8e9 light-green lighten-2">
-            <i class="material-icons">beach_access</i>Zone 9
+          <div className="collapsible-header #f1f8e9 light-green lighten-2">
+            <i className="material-icons">beach_access</i>Zone 9
           </div>
-          <div class="collapsible-body #f1f8e9 light-green lighten-5">
+          <div className="collapsible-body #f1f8e9 light-green lighten-5">
             <span>
               Kohala Volcano, which last erupted over 60,000 years ago.
             </span>
