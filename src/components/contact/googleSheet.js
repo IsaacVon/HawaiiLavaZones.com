b47b788622c0a18b433a5ffa8c5b96b3bb314f40
Isaac Householder <isaachouseholder@gmail.com>
2020-07-28 16:13:08 -0700

    removed some classes

--- src/components/contact/googleSheet.js
+++ src/components/contact/googleSheet.js
@@ -1,4 +1,5 @@
-import React from "react";
+
+
 
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const doc = new GoogleSpreadsheet(
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Home extends Component {
-  state = {};
-
-  render() {
-
+function Home () {
     return (
       <>
         <h1>Home</h1>
       </>
     );
-  }
+
 }
 
 export default Home;
