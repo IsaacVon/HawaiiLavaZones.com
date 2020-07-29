cd526a28ba9dd224b79bf7ef6c5b2fe67d781e00
Isaac Householder <isaachouseholder@gmail.com>
2020-07-29 15:25:52 -0700

    restructuring addressToZone

--- src/utils/addressToZone.js
+++ src/utils/addressToZone.js
@@ -1,44 +1,57 @@
-import googleGeoAPI from "../apiKeys"
+import googleGeoAPI from "../apiKeys";
 
 function addressToZone(address) {
+  const geoCoordinatesURL = buildGeoCoordinatesURL(address);
+  const geoCoordinates = requestGeoCoordinates(geoCoordinatesURL);
+  // Need to add rest of functions this way... stuck on getting requestGeoCoordinates to return its data
+}
+
+function buildGeoCoordinatesURL(address) {
   address = encodeURI(address);
-
-
-  // Geo Converter
-  // Input: Address
-  // Output: geoLocationLat, geoLocationLng
-
-  let apiKey = googleGeoAPI.googleGeoAPI;
-  let geoConvertUrl =
+  const apiKey = googleGeoAPI.googleGeoAPI;
+  const geoConvertUrl =
     "https://maps.googleapis.com/maps/api/geocode/json?address=" +
     address +
     "&key=" +
     apiKey;
+  return geoConvertUrl;
+}
 
+function requestGeoCoordinates(geoConvertUrl) {
   fetch(geoConvertUrl)
     .then((response) => response.json())
-    .then((data) => {
-      let geoLocationLat = data.results[0].geometry.location.lat;
-      let geoLocationLng = data.results[0].geometry.location.lng;
-      let lavaZoneUrl =
-        "https://geodata.hawaii.gov/arcgis/rest/services/Hazards/MapServer/3/query?where=1%3D1&outFields=hzone&geometry=" +
-        geoLocationLng +
-        "%2C" +
-        geoLocationLat +
-        "&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&returnGeometry=false&returnDistinctValues=true&outSR=4326&f=json";
-
-      fetch(lavaZoneUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          let lavaZone = data.features[0].attributes.hzone;
-          console.log("Lava Zone " + lavaZone);
-          console.log("Lat: ", geoLocationLat, "Lng: ", geoLocationLng);
-
-          return "Working"; // want to return lavaZone, geoLocationLat, geoLocationLng
-        })
-        .catch((e) => console.log(e));
-    })
+    .then((data) => buildLavaZoneURL(data))
     .catch((e) => console.log(e));
+}
+
+function buildLavaZoneURL(data) {
+  let geoLocationLat = data.results[0].geometry.location.lat;
+  let geoLocationLng = data.results[0].geometry.location.lng;
+  console.log("Lat: ", geoLocationLat);
+  console.log("Lng: ", geoLocationLng);
+
+  let lavaZoneUrl =
+    "https://geodata.hawaii.gov/arcgis/rest/services/Hazards/MapServer/3/query?where=1%3D1&outFields=hzone&geometry=" +
+    geoLocationLng +
+    "%2C" +
+    geoLocationLat +
+    "&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&returnGeometry=false&returnDistinctValues=true&outSR=4326&f=json";
+
+  requestLavaZone(lavaZoneUrl);
+}
+
+function requestLavaZone(lavaZoneUrl) {
+  fetch(lavaZoneUrl)
+    .then((response) => response.json())
+    .then((data) => updateLavazoneState(data))
+    .catch((e) => console.log(e));
+}
+
+function updateLavazoneState(data) {
+  let lavaZone = data.features[0].attributes.hzone;
+  console.log("Lava Zone", lavaZone);
+
+  return lavaZone;
 }
 
 export { addressToZone };
