66c2b3e09355e36ed09103a6e334042b64573013
Isaac Householder <isaachouseholder@gmail.com>
2020-11-04 21:28:02 -0800

    home page and search page done

--- src/components/desktop/navDesktop.jsx
+++ src/components/desktop/navDesktop.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
 
 import navBug from "../../assets/bug.png";
 
--- src/pages/desktop/homeDesktop.jsx
+++ src/pages/desktop/homeDesktop.jsx
@@ -1,12 +1,93 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
-function Home() {
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+
+import bug from "../../assets/bug.png";
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    fontSize: "20px",
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "400",
+    align: "center",
+    height: "62px",
+    width: "212px",
+    // width: "183px",
+    borderRadius: "20px",
+    color: "white",
+    backgroundColor: "#F89143",
+    boxShadow: "none",
+
+    "&:hover": {
+      backgroundColor: "#E27725",
+      boxShadow: "none",
+    },
+  },
+}))(Button);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+
   return (
     <>
-    <h1>Desktop Home</h1>
-
+      <div className="desktopHomeContainer">
+        <div className="desktopHomeLogoContainer">
+          <img className="desktopBugHome" src={bug} alt="HawaiiLavaZone" />
+          <div>
+            {" "}
+            <span
+              style={{
+                fontWeight: "100",
+                fontSize: "28px",
+              }}
+            >
+              HAWAII
+            </span>
+            <span
+              style={{
+                fontWeight: "normal",
+                fontSize: "28px",
+              }}
+            >
+              LAVA
+            </span>
+            <span
+              style={{
+                fontWeight: "100",
+                fontSize: "28px",
+              }}
+            >
+              ZONES
+            </span>
+          </div>
+          <div className="desktopIntroContainer">
+            The Big Island of Hawaii is divided into Lava Zones. When buying
+            property here, the Lava Zone is very important to know.{" "}
+          </div>
+          <div>
+            {" "}
+            <ColorButton
+              variant="contained"
+              color="primary"
+              className={classes.margin}
+              component={Link}
+              to="/Search"
+            >
+              Check Address
+            </ColorButton>
+          </div>
+        </div>
+        <div className="navSpacer"></div>
+      </div>
     </>
   );
 }
-
-export default Home;
